5bd1cf55cd84e513cba1b2409d808fa8529c66f4
Justin Lettau <justin.b.lettau@gmail.com>
2021-08-20 18:40:11 -0700

    chore: update dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,7 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
+    'prettier',
   ],
   rules: {
     '@typescript-eslint/explicit-module-boundary-types': 'off',
